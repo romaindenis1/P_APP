70980c98bac6d7fcad10dc2e17e6f99bc385e013
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-01-24 11:25:49 +0100

    Create(LoginPage)

--- Website/nodejswebshop/nodejswebshop/app/routes/User.js
+++ Website/nodejswebshop/nodejswebshop/app/routes/User.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
+
+const path = require("path");
 
 const router = express.Router();
 const controller = require("../controllers/UserController");
-router.get('/', controller.get) 
-module.exports = router; 
+
+router.get("/login", (req, res) => {
+  res.render("login", { name: "Romain" });
+});
+
+router.get("/register", (req, res) => {
+  res.render("register", { name: "Romain" });
+});
+
+router.get("/", controller.get);
+
+module.exports = router;
--- Website/nodejswebshop/nodejswebshop/app/server.js
+++ Website/nodejswebshop/nodejswebshop/app/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
-
 const app = express();
-const userRoute = require('./routes/User');
-app.use('/user', userRoute);
+const userRoute = require("./routes/User");
+app.use("/user", userRoute);
 
-
+app.set("view engine", "ejs");
 
 // Démarrage du serveur
 app.listen(8080, () => {
-    console.log('Server running on port 8080');
+  console.log("Server running on port 8080");
 });
+
+app.use(express.static(__dirname + "/public"));
