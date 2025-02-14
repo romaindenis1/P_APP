8dd9b26c06c4d8253e062822f2e9d7458aa0993b
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-02-14 11:49:09 +0100

    Change(Routes)
    - Removed the /user/ from the URL, it was annoying me

--- Website/nodejswebshop/app/server.js
+++ Website/nodejswebshop/app/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const jwt = require("jsonwebtoken");
 const path = require("path");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/User");
@@ -12,7 +13,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", accueilRouter);
-app.use("/user", userRoutes);
+app.use("/", userRoutes);
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
